1b0065736351956657a46f8a5a31ee197be47103
Pikarestiv <okwyodumodu@gmail.com>
2021-04-01 12:49:20 +0100

    Toggle advanced search functionality on and off

--- components/FormMainFilters/index.js
+++ components/FormMainFilters/index.js
@@ -12,6 +12,7 @@ import "react-input-range/lib/css/index.css";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import InputUnit from "../InputUnit";
 import { getApiLink, snakeCaseToText, tagToText } from "../../utils";
+import Link from '@material-ui/core/Link'
 
 import "./styles.scss";
 
@@ -28,6 +29,19 @@ const styles = theme => ({
     boxShadow: "0 0 20px rgba(0, 0, 0, 0.17)",
     borderRadius: "15px",
     width: "100%"
+  },
+  advSearch: {
+    // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    background: '#eb613d',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 32,
+    padding: '0 30px',
+  },
+  advSearchBox: {
+    width: '100%',
   }
 });
 const inputTheme = createMuiTheme({});
@@ -78,17 +92,17 @@ class FormMainFilters extends PureComponent {
     const { title, body, tag_groups, tag_groups_values } = this.state;
 
     return (
-      // <Paper
-      //   style={{
-      //     display: 'flex',
-      //     justifyContent: 'felx-start',
-      //     alignItems: 'center',
-      //     flexWrap: 'wrap',
-      //   }}
-      //   className="main-filters"
-      // >
-      //   <h1>Search for Grants</h1>
-
+      <Paper
+        style={{
+          display: 'flex',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+          flexWrap: 'wrap',
+        }}
+        className="main-filters"
+      >
+        <h1>Search for Grants</h1>
+        
       <div className="search">
         <MuiThemeProvider theme={inputTheme}>
           <TextField
@@ -116,60 +130,74 @@ class FormMainFilters extends PureComponent {
         />
       </div>
 
-      //   <div className="filters-block">
-      //     <TextField
-      //       id="search-title"
-      //       label="Search in title"
-      //       value={title}
-      //       onChange={this.handleChange('title')}
-      //     />
-      //     <TextField
-      //       id="search-body"
-      //       label="Search in body"
-      //       value={body}
-      //       onChange={this.handleChange('body')}
-      //     />
-      //   </div>
-      //   <div className="filters-block">
-      //     <InputUnit classes={classes} onChange={selectedUnit => this.setState({ selectedUnit })} />
-      //     {
-      //       tag_groups && tag_groups.length > 0 && (
-      //         tag_groups.map(tag_group =>
-      //           tag_group.tag && tag_group.tag.length > 0 &&
-      //           <FormControl key={tag_group.id} className={classes.formControl}>
-      //             <InputLabel htmlFor={`selected-${tag_group.title}`}>
-      //               {snakeCaseToText(tag_group.title)}
-      //             </InputLabel>
-      //             <Select
-      //               value={tag_groups_values[tag_group.title] || ''}
-      //               onChange={(e) =>
-      //                 this.setState({
-      //                   tag_groups_values: { ...tag_groups_values, [tag_group.title]: e.target.value }
-      //                 })}
-      //               input={<Input name={`selected-${tag_group.title}`} id={`selected-${tag_group.title}`} />}
-      //             >
-      //               <MenuItem value="">
-      //                 <em>Not specified</em>
-      //               </MenuItem>
-      //               {tag_group.tag
-      //                 .sort((a, b) => a.title > b.title ? 1 : -1)
-      //                 .map(e => <MenuItem key={e.title} value={e.id}>{tagToText(e.title)}</MenuItem>)}
-      //             </Select>
-      //           </FormControl>
-      //         )
-      //       )
-      //     }
-      //   </div>
-      //   <div className="btn-wrapper">
-      //     <Button
-      //       variant="contained"
-      //       color="primary"
-      //       onClick={() => onSubmit(this.state)}
-      //     >
-      //       Search
-      //     </Button>
-      //   </div>
-      // </Paper>
+        <div className={classes.advSearchBox}>
+          <Link
+            onClick={() => {
+              let advSearchEl = document.getElementById('advanced-search');
+              advSearchEl.style.display = advSearchEl.style.display != "none"?"none":"block";
+            }}
+            >
+              <button className={classes.advSearch}>Advanceed Search</button>
+          </Link>
+        </div>
+
+        <div id="advanced-search" style={{display: 'none'}}>
+          <div className="filters-block">
+            <TextField
+              id="search-title"
+              label="Search in title"
+              value={title}
+              onChange={this.handleChange('title')}
+            />
+            <TextField
+              id="search-body"
+              label="Search in body"
+              value={body}
+              onChange={this.handleChange('body')}
+            />
+          </div>
+
+          <div className="filters-block">
+            <InputUnit classes={classes} onChange={selectedUnit => this.setState({ selectedUnit })} />
+            {
+              tag_groups && tag_groups.length > 0 && (
+                tag_groups.map(tag_group =>
+                  tag_group.tag && tag_group.tag.length > 0 &&
+                  <FormControl key={tag_group.id} className={classes.formControl}>
+                    <InputLabel htmlFor={`selected-${tag_group.title}`}>
+                      {snakeCaseToText(tag_group.title)}
+                    </InputLabel>
+                    <Select
+                      value={tag_groups_values[tag_group.title] || ''}
+                      onChange={(e) =>
+                        this.setState({
+                          tag_groups_values: { ...tag_groups_values, [tag_group.title]: e.target.value }
+                        })}
+                      input={<Input name={`selected-${tag_group.title}`} id={`selected-${tag_group.title}`} />}
+                    >
+                      <MenuItem value="">
+                        <em>Not specified</em>
+                      </MenuItem>
+                      {tag_group.tag
+                        .sort((a, b) => a.title > b.title ? 1 : -1)
+                        .map(e => <MenuItem key={e.title} value={e.id}>{tagToText(e.title)}</MenuItem>)}
+                    </Select>
+                  </FormControl>
+                )
+              )
+            }
+          </div>
+          <div className="btn-wrapper">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => onSubmit(this.state)}
+            >
+              Search
+            </Button>
+          </div>
+        </div>
+      </Paper>
     );
   }
 }
